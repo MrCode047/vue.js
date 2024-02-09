8fc2dececd3ba5ab896d656fe65ab23d7261455b
Jory Aguet <142528473+MrCode047@users.noreply.github.com>
2024-02-09 18:28:37 +0000

    -

---
+++ 2_project_vue/src/components/PrimeiroComponente.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Ola Vue!</h1>
+        <p>Meu nome é {{ nome }} e trabalho como {{ profissão }}</p> 
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PrimeiroComponente',
+    data() {
+        return {
+            nome: "Jory",
+            profissão: "Programador"
+        }
+    }
+}
+</script>
